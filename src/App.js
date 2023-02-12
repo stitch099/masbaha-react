12dc6e261fb86c90f3ab7fa5a40b5fde7a8b24db
leen <leen.shehab2996@gmail.com>
2023-02-12 11:43:58 -0500

    working code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
-import React from "react";
-import Counter from "./Counter";
+import React, { useState, useEffect, useRef } from "react";
+import "./style.css";
+import Counter from "./Message";
+import {
+  WhatsappShareButton,
+  WhatsappIcon,
+} from "react-share";
+
+const LOCAL_STORAGE_KEY = "counter";
 
 function App() {
+  const shareUrl = 'https://masbaha2.web.app/';
+
+  const [ data, setData ] =  useState([
+    { title: 'hamdellah', id: Math.floor(Math.random() * 1000000) },
+    { title: 'subhanallah', id: Math.floor(Math.random() * 1000000) },
+    { title: 'laa elah ela allah', id: Math.floor(Math.random() * 1000000) },
+  ]);
+
+  const newData = useRef();
+
+  useEffect(() => {
+    let storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (storedData) {
+      try{
+        setData(JSON.parse(storedData));
+      }catch(e){
+        console.log(e);
+      }
+    }else{
+      setData(data)
+    }
+  }, [])
+  
+  const toggleVisibility = (id) => {
+    const dataNew = data.filter((item) => item.id !== id);
+    setData(dataNew);
+  };
+
+  const handleAddCounter = () => {
+    let title = newData.current.value;
+    let newCounter = { title: title, id: Math.floor(Math.random() * 1000000) };
+    if (title === "") { 
+      alert("Please enter a tasbeeh");
+    }else{
+      let counter = [...data, newCounter];
+      setData(counter);
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(counter));
+    }
+    newData.current.value = null;
+  }
+
+
+
   return (
     <>
-      <Counter />   
+      <WhatsappShareButton url={shareUrl}>
+        <WhatsappIcon size={32} round />
+      </WhatsappShareButton>
+      <br />
+      <input ref={newData} type="text" />
+      <button onClick={handleAddCounter}>add</button>
+      {data.map((e, i) => {
+        return <Counter 
+        key={e.id} 
+        title={e.title} 
+        id={e.id} 
+        index={i}
+        toggleVisibility={toggleVisibility} 
+        />
+      })}
     </>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function Counter() {
+const Counter = (props) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let saved = localStorage.getItem(props.id);
+    if (saved) {
+      try{
+        setCount(JSON.parse(saved));
+      }catch(e){
+        console.log(e);
+      }
+    }
+ }, [])
+
+  const increment = () => {
+    let newCount = count + 1;
+    setCount(newCount);
+    localStorage.setItem(props.id, JSON.stringify(newCount));
+  }
+
+    const reset = () => {
+    let newCount = 0;
+    setCount(newCount);
+    localStorage.setItem(props.id, JSON.stringify(newCount));
+  }
   return (
-    <div>Counter</div>
-  )
-}
+    <>
+    <hr />
+    <h2>{props.title}</h2>
+    <br />
+    <span>clicks: {count}</span>
+    <br />
+   <button onClick={increment}>increment</button> 
+   <br />
+   <button onClick={reset}>reset</button>
+   <br />
+   <button onClick={() => props.toggleVisibility(props.id)}>cancel</button>
+    </>
+  );
+};
+export default Counter;
